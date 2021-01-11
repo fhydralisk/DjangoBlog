89380de35028d1ad47051cbc6da00dcec051ce05
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2021-01-11 21:43:59 +0800

    guild storage app.

--- guild_storage/admin.py
+++ guild_storage/admin.py
@@ -18,3 +18,8 @@ class GuildMaterialJournalAdmin(admin.ModelAdmin):
     list_display = (
         'material_input', 'amount_input', 'input_from', 'material_output', 'amount_output', 'output_to', 'comment'
     )
+
+    def delete_queryset(self, request, queryset):
+        # delete it one by one, to trigger model's delete method.
+        for obj in queryset:
+            obj.delete()
